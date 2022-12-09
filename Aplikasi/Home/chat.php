2b7ad4aeec082f6d053ce606194e913a1beb6385
Ratusukmakomala <92583035+Ratusukmakomala@users.noreply.github.com>
2022-12-09 10:31:03 +0700

    chat

---
+++ Aplikasi/Home/chat.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/637b2036daff0e1306d88499/1gicfblsg';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+  <body>
+    <h1>Silakan Anda Bisa Melakukan Konsultasi Dengan Dokter</h1>
+    <img src="konsultasi.jpeg" alt="" srcset="">
+    <p>Anda bisa melakukan konsultasi mengenai keluhan penyakit anda pada fitur live chat disebelah kanan</p>
+
+  </body>
+
+</html>
