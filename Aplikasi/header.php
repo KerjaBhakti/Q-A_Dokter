2149702ea9c58809d3189936e1d75ef747e663f1
isnaenti <100338225+isnaenti@users.noreply.github.com>
2022-12-19 13:48:01 +0700

    header

---
+++ Aplikasi/header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf8" />
+<link href="main.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/635f6e6ddaff0e1306d4d680/1ggmcenq2';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<body>
+        <div class="header">
+ <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKQfHfAh2Ke9IFTbtiwzQ93GUyXz12es6eGw&usqp=CAU" class="logo"/></a>
+  <div class="header-right">
+    <!--<a class="active mitem" href="/index.php">Home</a>-->
+    
+
+
+     <!--      <div class="dropdown">
+    <a href="#" class="mitem dropdownbtn">Calls
+      <i class="fa fa-caret-down"></i>
+    </a>
+
+
+    <div class="dropdown-content">
+      <a href="/createsandbox.php" class="mitem">Create Sandbox</a>
+     <a href="/exportsandbox.php" class="mitem">Export Sandbox</a>
+     <a href="/deletesandbox.php" class="mitem">Delete Sandbox</a>-->
+      <!--<a href="/createsandboxuser.php" class="mitem">Create Sandbox User</a>-->
+    </div>
+  </div>
+
+    <!--<a href="https://wa.me/6281214704713" target="_blank" class="mitem">Contact</a>-->
+   <!-- <a href="https://developer.nbg.gr/" target="_blank" class="mitem">Portal</a>-->
+      <!--<a href="https://github.com/myNBGcode" target="_blank" class="mitem">Github</a>-->
+  </div>
+</div>
+    </body>
+</html>
