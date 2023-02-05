c64799ea5908ea282102982d06d76552558efed4
Ratusukmakomala <92583035+Ratusukmakomala@users.noreply.github.com>
2023-02-05 10:50:36 +0700

    chat

---
+++ Aplikasi/Home/Crud_janji/chat.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Konsultasi</title>
+  </head>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63c256e8c2f1ac1e202d62a7/1gmni70p3';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<center>
+  <body>
+    <h1>Silakan Anda Bisa Melakukan Konsultasi Dengan Dokter</h1>
+    <img src="konsultasi.jpeg" alt="" srcset="">
+    <p>Anda bisa melakukan konsultasi mengenai keluhan penyakit anda pada fitur live chat disebelah kanan</p>
+
+  </body>
+ </center>
+</html>
